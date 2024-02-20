dbe21126a3531977bf2545956733c33c7fea4994
Andeskjerf <predgd@pm.me>
2024-02-20 11:15:19 +0100

    print percentage & round

--- hvaGjørKoden/Program.cs
+++ hvaGjørKoden/Program.cs
@@ -5,23 +5,32 @@ string text = "something";
 // Så lenge input ikke er tomt, kjør loop
 while (!string.IsNullOrWhiteSpace(text))
 {
-		// Hent input fra bruker
+    // Hent input fra bruker
     text = Console.ReadLine();
-		// Gå over hver karakter i input
+    // Gå over hver karakter i input
     foreach (char character in text ?? string.Empty)
     {
-				// For hver karakter vi møter på, lagre hvor ofte vi har sett den i arrayet
+        // For hver karakter vi møter på, lagre hvor ofte vi har sett den i arrayet
         counts[(int)Char.ToUpper(character)]++;
     }
-		// Istedenfor å hente counts.Length, så bruker vi range for å loope over counts arrayet
+    int[] percentages = new int[range];
+    int count = 0;
     for (var i = 0; i < range; i++)
     {
-				// Vi vil bare printe noe hvis vi har møtt på karakteren mer enn 0 ganger
         if (counts[i] > 0)
         {
-						// Print karakteren og hvor ofte vi har møtt på den
-            var character = (char)i;
-            Console.WriteLine(character + " - " + counts[i]);
+            percentages[i] = counts[i];
+            count += counts[i];
+        }
+    }
+
+    for (int i = 0; i < range; i++)
+    {
+        if (percentages[i] > 0)
+        {
+            double percentage = ((double)percentages[i] / (double)count) * 100.0;
+            char character = (char)i;
+            Console.WriteLine(character + " - " + Math.Round(percentage));
         }
     }
 }
