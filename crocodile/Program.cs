99cdcf9bcc7e3e9c00c145d4578b5be6cc74ade8
Andeskjerf <predgd@pm.me>
2024-02-20 10:27:24 +0100

    crocodile

---
+++ crocodile/Program.cs
@@ -0,0 +1,29 @@
+﻿char input = '0';
+int firstNum = -10;
+int secondNum = -10;
+
+Random rnd = new Random();
+
+while (true)
+{
+    while (firstNum == -10 && secondNum == -10 || firstNum == secondNum)
+    {
+        firstNum = rnd.Next(0, 100);
+        secondNum = rnd.Next(0, 100);
+        Console.WriteLine($"{firstNum} or {secondNum}?");
+    }
+
+    input = Console.ReadKey().KeyChar;
+		Console.WriteLine();
+
+    if (input == '<' && firstNum < secondNum || input == '>' && firstNum > secondNum)
+    {
+        Console.WriteLine("Correct! Assigning new numbers...");
+        firstNum = -10;
+        secondNum = -10;
+    }
+    else
+    {
+        Console.WriteLine("Wrong!");
+    }
+}
