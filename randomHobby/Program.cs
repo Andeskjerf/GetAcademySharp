bb14fec0548d4d53d80f68f38bd4eff51de21b8c
Andeskjerf <predgd@pm.me>
2024-02-20 09:49:39 +0100

    hobby project

---
+++ randomHobby/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] hobbies = {
+    "Hiking",
+    "Reading",
+    "Swimming",
+    "Painting",
+    "Running",
+};
+
+string input = "";
+
+while (true)
+{
+    input = Console.ReadLine();
+    if (input == "cancel" || input == null)
+    {
+        break;
+    }
+    // set first letter uppercase
+    string after = input.Substring(0, 1).ToUpper() + input.Substring(1);
+    string hobby = hobbies[new Random().Next(hobbies.Length)];
+
+    Console.WriteLine(after + " likes to partake in " + hobby + "!");
+}
