c77522fe2f4f4325ac5890b9955094d5564bb9bc
Andeskjerf <predgd@pm.me>
2024-02-23 10:51:44 +0100

    study points, list of courses

--- studentAdministrasjon/Program.cs
+++ studentAdministrasjon/Program.cs
@@ -5,14 +5,14 @@
         private int _id;
         private string _name;
         private int _age;
-        private Course _course;
+        private Course[] _courses;
 
-        public Student(int id, string name, int age, Course course)
+        public Student(int id, string name, int age, Course[] courses)
         {
             this._id = id;
             this._name = name;
             this._age = age;
-            this._course = course;
+            this._courses = courses;
         }
 
         public string getName()
@@ -20,9 +20,33 @@
             return this._name;
         }
 
+        public string printCourses()
+        {
+            string courses = "";
+            for (int i = 0; i < _courses.Length; i++)
+            {
+                courses += _courses[i].getName();
+                if (i < _courses.Length - 1)
+                {
+                    courses += ", ";
+                }
+            }
+            return courses;
+        }
+
+        public int getStudyPoints()
+        {
+            int points = 0;
+            for (int i = 0; i < _courses.Length; i++)
+            {
+                points += _courses[i].getPoints();
+            }
+            return points;
+        }
+
         public void print()
         {
-            Console.WriteLine($"ID: {_id}, Name: {_name}, Age: {_age}, Course: {_course.getName()}");
+            Console.WriteLine($"ID: {_id}, Name: {_name}, Age: {_age}, Courses: {printCourses()}");
         }
     }
 
@@ -42,6 +66,11 @@
         public string getName()
         {
             return this._name;
+        }
+
+        public int getPoints()
+        {
+            return this._points;
         }
 
         public void print()
@@ -76,28 +105,36 @@
         static Student _student1;
         static Student _student2;
 
-        static Course _course1;
-        static Course _course2;
+        static Course[] _courses;
 
         static Grade _grade1;
         static Grade _grade2;
+        static Grade _grade3;
+        static Grade _grade4;
+        static Grade _grade5;
+        static Grade _grade6;
 
         static Program()
         {
-            _course1 = new Course(1, "Math", 10);
-            _course2 = new Course(2, "Science", 5);
+            _courses = new Course[] {
+                                new Course(1, "Math", 10),
+                                new Course(2, "Science", 5),
+                                new Course(3, "English", 15),
+                        };
 
-            _student1 = new Student(1, "John", 20, _course1);
-            _student2 = new Student(2, "Jane", 21, _course2);
+            _student1 = new Student(1, "John", 20, _courses);
+            _student2 = new Student(2, "Jane", 21, _courses);
 
-            _grade1 = new Grade(_student1, _course1, 5);
-            _grade2 = new Grade(_student2, _course2, 5);
+            _grade1 = new Grade(_student1, _courses[0], 3);
+            _grade2 = new Grade(_student1, _courses[1], 6);
+            _grade3 = new Grade(_student1, _courses[2], 4);
+            _grade4 = new Grade(_student2, _courses[0], 2);
+            _grade5 = new Grade(_student2, _courses[1], 4);
+            _grade6 = new Grade(_student2, _courses[2], 5);
         }
 
         static void Main(string[] args)
         {
-            _course1.print();
-            _course2.print();
             _student1.print();
             _student2.print();
             _grade1.print();
